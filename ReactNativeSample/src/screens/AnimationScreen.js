ec2d86efb5cc3d19fe022e376d4b1d667f1bac6d
Erwan Masson <airone.masson@gmail.com>
2017-07-20 11:44:32 +0200

    Eslint added

--- ReactNativeSample/src/screens/AnimationScreen.js
+++ ReactNativeSample/src/screens/AnimationScreen.js
@@ -5,13 +5,58 @@ export const ANIMATION_SCENE_NAME = 'ANIMATION_SCENE';
 
 export default class AnimationScreen extends Component {
 
-  static navigationOptions = {
-    title: 'Animation',
-  };
+    static navigationOptions = {
+        title: 'Animation',
+    };
+    
+    constructor () {
+        super()
+        this.spinValue = new Animated.Value(0)
+    }
+  
+    componentDidMount () {
+        this.spin()
+    }
 
-  render() {
-    return (
-      <SampleState />
-    );
-  }
+    spin () {
+        this.spinValue.setValue(0)
+        Animated.timing(
+        this.spinValue,
+        {
+            toValue: 1,
+            duration: 4000,
+            easing: Easing.linear,
+            useNativeDriver: true
+        },
+        ).start(() => this.spin())
+    }
+
+    render() {
+        const spin = this.spinValue.interpolate({
+            inputRange: [0, 1],
+            outputRange: ['0deg', '360deg']
+        })
+        return (
+        <View style={styles.container}>
+            <Animated.Image
+                style={{
+                    flex: 1, 
+                    width: null, 
+                    height: null, 
+                    resizeMode: 'contain',
+                    transform: [{rotate: spin}]}}
+                source={{uri: 'https://pics.me.me/random-beaver-shot-mematic-net-https-t-co-17pssxlfen-11233447.png'}}
+            />
+        </View>
+        );
+    }
+    
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
